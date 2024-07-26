d7219ab06c3ee4fe7be05a50a2fe9574dde27958
Utkarsh <ignition3951@gmail.com>
2024-07-26 20:35:22 +0530

    Initial commit spring security in action

---
+++ spring-security/src/main/java/com/utk/TacoCloudClient1Application.java
@@ -0,0 +1,13 @@
+package com.utk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudClient1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudClient1Application.class, args);
+	}
+
+}
