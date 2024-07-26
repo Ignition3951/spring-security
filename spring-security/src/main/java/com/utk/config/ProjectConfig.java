5f5a98b54a28213e9ffa78dbad6dce4e62663985
Utkarsh <ignition3951@gmail.com>
2024-07-26 23:03:52 +0530

    Play with authorizeHttpRequests

--- spring-security/src/main/java/com/utk/config/ProjectConfig.java
+++ spring-security/src/main/java/com/utk/config/ProjectConfig.java
@@ -2,11 +2,14 @@ package com.utk.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
@@ -21,5 +24,12 @@ public class ProjectConfig {
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.httpBasic(Customizer.withDefaults());
+		httpSecurity.authorizeHttpRequests(requests -> requests.anyRequest().permitAll());
+		return httpSecurity.build();
 	}
 }
