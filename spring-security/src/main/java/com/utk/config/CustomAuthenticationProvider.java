d21ed0868454fda0df57d44aee276a71143d1054
Utkarsh <ignition3951@gmail.com>
2024-07-26 23:24:13 +0530

    Use custom Authentication provider

---
+++ spring-security/src/main/java/com/utk/config/CustomAuthenticationProvider.java
@@ -0,0 +1,31 @@
+package com.utk.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+@Configuration
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		String username = authentication.getName();
+		String credentials = String.valueOf(authentication.getCredentials());
+		if ("utkarsh".equals(username) && "password123".equals(credentials)) {
+			return new UsernamePasswordAuthenticationToken(username, credentials, Arrays.asList());
+		} else {
+			throw new BadCredentialsException("Kindly check the credentials!!!!!!");
+		}
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+	}
+
+}
--- spring-security/src/main/java/com/utk/config/ProjectConfig.java
+++ spring-security/src/main/java/com/utk/config/ProjectConfig.java
@@ -1,35 +1,38 @@
 package com.utk.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
 
-	@Bean
-	UserDetailsService userDetailsService(PasswordEncoder encoder) {
-		String password = encoder.encode("password");
-		User user = (User) User.withUsername("utkarsh").password(password).authorities("read").build();
-		return new InMemoryUserDetailsManager(user);
-	}
+//	@Bean
+//	UserDetailsService userDetailsService(PasswordEncoder encoder) {
+//		String password = encoder.encode("password");
+//		User user = (User) User.withUsername("utkarsh").password(password).authorities("read").build();
+//		return new InMemoryUserDetailsManager(user);
+//	}
+//
+//	@Bean
+//	PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
 
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+	@Autowired
+	private CustomAuthenticationProvider authenticationProvider;
 
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.httpBasic(Customizer.withDefaults());
 		httpSecurity.authorizeHttpRequests(requests -> requests.anyRequest().permitAll());
 		return httpSecurity.build();
+	}
+
+	public ProjectConfig(CustomAuthenticationProvider authenticationProvider) {
+		this.authenticationProvider = authenticationProvider;
 	}
 }
