cd041a74d528709c85948c966edd1b8bd1248439
Utkarsh <ignition3951@gmail.com>
2024-07-26 22:53:37 +0530

    Use bCrypt to encode password and use

--- spring-security/src/main/java/com/utk/config/ProjectConfig.java
+++ spring-security/src/main/java/com/utk/config/ProjectConfig.java
@@ -4,14 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectConfig {
 
 	@Bean
-	UserDetailsService userDetailsService() {
-		User user = (User) User.withUsername("utkarsh").password("password").authorities("read").build();
+	UserDetailsService userDetailsService(PasswordEncoder encoder) {
+		String password = encoder.encode("password");
+		User user = (User) User.withUsername("utkarsh").password(password).authorities("read").build();
 		return new InMemoryUserDetailsManager(user);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
