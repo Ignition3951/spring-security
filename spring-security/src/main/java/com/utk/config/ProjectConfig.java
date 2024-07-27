c3376a9a84c060e1fac757d4efbcb9ae3fd6ddd3
Utkarsh <ignition3951@gmail.com>
2024-07-27 14:36:16 +0530

    Use JPA later and check CustomFilter logic of request header

--- spring-security/src/main/java/com/utk/config/ProjectConfig.java
+++ spring-security/src/main/java/com/utk/config/ProjectConfig.java
@@ -10,21 +10,34 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import com.utk.filter.RequestValidationFilter;
 import com.utk.model.User;
-import com.utk.service.InMemoryUserDetailsService;
 
 @Configuration
 public class ProjectConfig {
 
 	@Bean
 	UserDetailsService userDetailsService(PasswordEncoder encoder) {
-		String password = encoder.encode("password");
+		String password = encoder.encode("12345");
 		UserDetails user = new User("utk1311", password, "read");
 		List<UserDetails> userDetails = List.of(user);
-		return new InMemoryUserDetailsService(userDetails);
+		return new InMemoryUserDetailsManager(userDetails);
 	}
+
+//	@Bean
+//	UserDetailsService userDetailsService(DataSource dataSource) {
+//		String usersByUsernameQuery = "select username, password, enabled from users where username =  'john' ";
+//		// String authByUsersQuery = "select username, authority from authorities where
+//		// username = ?";
+//		UserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+//		manager.userExists(usersByUsernameQuery);
+//		manager.loadUserByUsername(usersByUsernameQuery);
+//		return manager;
+//	}
 
 	@Bean
 	PasswordEncoder passwordEncoder() {
@@ -36,6 +49,7 @@ public class ProjectConfig {
 
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.addFilterBefore(new RequestValidationFilter(), BasicAuthenticationFilter.class);
 		httpSecurity.httpBasic(Customizer.withDefaults());
 		// httpSecurity.authenticationProvider(authenticationProvider);
 		httpSecurity.authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
