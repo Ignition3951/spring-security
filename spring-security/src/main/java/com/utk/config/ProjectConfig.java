6b67c65737c655feac64a6f7597993b4adf74759
Utkarsh <ignition3951@gmail.com>
2024-07-28 20:58:24 +0530

    csrf token added to make post request from controller

--- spring-security/src/main/java/com/utk/config/ProjectConfig.java
+++ spring-security/src/main/java/com/utk/config/ProjectConfig.java
@@ -2,7 +2,6 @@ package com.utk.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CsrfFilter;
@@ -15,22 +14,21 @@ public class ProjectConfig {
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 //		httpSecurity.formLogin(Customizer.withDefaults());
-		// httpSecurity.formLogin(c -> c.defaultSuccessUrl("/home", true));
+		httpSecurity.formLogin(c -> c.defaultSuccessUrl("/main", true));
 		// httpSecurity.addFilterBefore(new RequestValidationFilter(),
 		// BasicAuthenticationFilter.class);
 		// httpSecurity.addFilterAfter(new AuthenticationLoggingFilter(),
 		// BasicAuthenticationFilter.class);
-		httpSecurity.httpBasic(Customizer.withDefaults());
+//		httpSecurity.httpBasic(Customizer.withDefaults());
 		// httpSecurity.authenticationProvider(customAuthenticationProvider);
-		// httpSecurity.authorizeHttpRequests(requests ->
-		// requests.anyRequest().authenticated());
+		httpSecurity.authorizeHttpRequests(requests -> requests.anyRequest().authenticated());
 		// httpSecurity.authorizeHttpRequests(requests ->
 		// requests.anyRequest().hasAuthority("write"));
 //		httpSecurity.authorizeHttpRequests(requests -> requests.anyRequest().hasRole("ADMIN"));
 //		httpSecurity.authorizeHttpRequests(requests -> requests.requestMatchers("/home").hasRole("ADMIN")
 //				.requestMatchers("/caio").hasRole("MANAGER").anyRequest().permitAll());
-		httpSecurity.addFilterAfter(new CsrfTokenLoggerFilter(), CsrfFilter.class)
-				.authorizeHttpRequests(request -> request.anyRequest().permitAll());
+		httpSecurity.addFilterAfter(new CsrfTokenLoggerFilter(), CsrfFilter.class);
+//				.authorizeHttpRequests(request -> request.anyRequest().permitAll());
 		return httpSecurity.build();
 	}
 }
---
+++ spring-security/src/main/java/com/utk/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.utk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/main")
+	public String mainController() {
+		return "main";
+	}
+}
