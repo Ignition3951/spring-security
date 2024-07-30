b5d0add9a6ceab52253e075b184acd257d1f9fbe
Utkarsh <ignition3951@gmail.com>
2024-07-30 21:40:33 +0530

    Implement CORS policy

--- spring-security/src/main/java/com/utk/controller/MainController.java
+++ spring-security/src/main/java/com/utk/controller/MainController.java
@@ -1,13 +1,33 @@
 package com.utk.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
+	private final Logger logger = LogManager.getLogger(MainController.class.getName());
+
 	@GetMapping("/main")
 	public String mainController() {
 		return "main";
+	}
+
+	@GetMapping("/homes")
+	public String homesController() {
+		return "home";
+	}
+
+	@PostMapping("/test")
+	@ResponseBody
+	@CrossOrigin("http://localhost:8081")
+	public String test() {
+		logger.info("Test method called");
+		return "HELLO";
 	}
 }
