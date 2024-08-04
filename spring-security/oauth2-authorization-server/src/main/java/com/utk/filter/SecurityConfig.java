1cffc1838e35bb264448c7caef3303b5bd11dcb9
Utkarsh <ignition3951@gmail.com>
2024-08-04 10:50:33 +0530

    Implement Opaque Tokens

--- spring-security/oauth2-authorization-server/src/main/java/com/utk/filter/SecurityConfig.java
+++ spring-security/oauth2-authorization-server/src/main/java/com/utk/filter/SecurityConfig.java
@@ -26,6 +26,8 @@ import org.springframework.security.oauth2.server.authorization.client.Registere
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -83,7 +85,13 @@ public class SecurityConfig {
 				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
 				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
 				.redirectUri("https://www.manning.com/authorized").scope(OidcScopes.OPENID).build();
-		return new InMemoryRegisteredClientRepository(registeredClient);
+		RegisteredClient registeredCredentialsClient = RegisteredClient.withId(UUID.randomUUID().toString())
+				.clientId("clientCred").clientSecret(passwordEncoder.encode("secret"))
+				.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+				.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+				.tokenSettings(TokenSettings.builder().accessTokenFormat(OAuth2TokenFormat.REFERENCE).build())
+				.scope("CUSTOM").build();
+		return new InMemoryRegisteredClientRepository(registeredClient, registeredCredentialsClient);
 	}
 
 	@Bean
